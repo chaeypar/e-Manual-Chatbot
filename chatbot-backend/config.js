a72ea858815a78cc7a1f26388da33c96b4be29e0
chaeypar <kidsland09@naver.com>
2023-07-20 15:20:38 +0900

    key

---
+++ chatbot-backend/config.js
@@ -0,0 +1,8 @@
+const configuration = {
+    organization: "org-56oMb3kieJuwqX05WFIQwPYW",
+    apiKey : "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"
+}
+
+//fine-tuned model : "davinci:ft-e-manual-chatbot-2023-07-18-10-34-13"
+
+export default configuration;
